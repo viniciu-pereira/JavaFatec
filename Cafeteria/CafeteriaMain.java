7ddb251c058d3650f564e1d5606e5f433a54155e
VINICIUS FELIPI PEREIRA <1091392323008@fatec-srv.local>
2023-10-21 11:04:25 -0300

    exercicios sobre padroes de projeto

--- Cafeteria/CafeteriaMain.java
+++ Cafeteria/CafeteriaMain.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CafeteriaMain {
 
     public static void main(String[] args) {
@@ -10,5 +13,10 @@ public class CafeteriaMain {
 
         Bebida conCafe = new ConCafe(expresso);
         System.out.println(conCafe.getDescricao() + conCafe.getValor());
+
+        List<Bebida> listaBebidas = new ArrayList<Bebida>();
+        list.add(expresso);
+        list.add(ConCafe);
+        list.add(ConLatte);
     }
 }
